52b9f751b8851cce24eb334f214f44c6c4bef18a
HPDell <huyg0180110559@outlook.com>
2022-03-18 16:10:12 +0000

    fix: some develop problem

--- myzone/settings.py
+++ myzone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7$)6&e-_p*_s5o_e(+638(vmrpveojftu4ldu5glmk5f39)yul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv('MYZONE_MODE', 'RELEASE').upper() == 'DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('MYZONE_HOST', 'localhost')
+]
 
 
 # Application definition
