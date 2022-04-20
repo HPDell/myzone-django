95fd2796fbd878f627d1a24c591b995853540877
HPDell <huyg0180110559@outlook.com>
2022-04-20 17:14:49 +0000

    add: publication list

--- myzoneapp/admin.py
+++ myzoneapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.db import models
-from .models import Category, Post, Profile, Tag
+from .models import Category, Post, Profile, Publication, Tag
 
 # Register your models here.
 
@@ -16,7 +16,12 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name_en', 'name_zh_cn')
 
 
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'publish_date')
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Profile)
+admin.site.register(Publication, PublicationAdmin)
